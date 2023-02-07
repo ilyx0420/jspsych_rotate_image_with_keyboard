71359b1a469643a8f9b29940782f1e6fecfcb873
ilyx0420 <71193065+ilyx0420@users.noreply.github.com>
2023-02-07 12:22:03 +0100

    Update jspsych-html-keyboard-response-touch.js

--- jspsych-6.3.1/plugins/jspsych-html-keyboard-response-touch.js
+++ jspsych-6.3.1/plugins/jspsych-html-keyboard-response-touch.js
@@ -61,7 +61,7 @@ jsPsych.plugins["html-keyboard-response-touch"] = (function() {
   plugin.trial = function(display_element, trial) {
 
     var new_html = '<div id="jspsych-html-keyboard-response-stimulus">'+trial.stimulus+'</div>';
-    var new_html = '<button ontouchstart="press me"></button>'
+    var new_html = '<div ontouchstart><button type="button">Click Me!</button>'
 
     // add prompt
     if(trial.prompt !== null){
